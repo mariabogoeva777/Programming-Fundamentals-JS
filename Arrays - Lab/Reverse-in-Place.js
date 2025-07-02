7b779229a1dbe3b4c77aba3d2625b4e01d4646fa
Maria Bogoeva <marriabogoeva@gmail.com>
2025-07-02 18:41:07 +0300

    Add files via upload
    1. Sum First and Last Array Elements
    2. Day of Week
    3. Reverse an Array of Numbers
    4. Reverse in Place
    5. Sum Even Numbers
    6. Even and Odd Subtraction
    7. Equal Arrays
    8. Condense Array to Number

---
+++ Arrays - Lab/Reverse-in-Place.js
@@ -0,0 +1,7 @@
+function reverseInPlace(arr){
+    arr.reverse();
+        console.log(arr.join(" "));
+}
+reverseInPlace(['abc', 'def', 'hig', 'klm', 'nop']);
+reverseInPlace(['33','123','0','dd']);
+reverseInPlace(['a', 'b', 'c', 'd', 'e']);
